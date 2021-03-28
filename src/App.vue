ed79b8a54c9b11f2a9dd4fdb3390c3e326b61063
hezongzhu <hezongzhu@gmail.com>
2021-03-29 01:08:48 +0800

    更新选选中样式

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,105 @@
 <template>
   <v-app id="inspire">
- <v-navigation-drawer
-      v-model="drawer"
-      app
+    <v-navigation-drawer
+        v-model="drawer"
+        app
     >
       <!--  -->
     </v-navigation-drawer>
-
-
     <v-app-bar
-      app
-      color="primary"
-      dark
+        app
+        color="primary"
+        dark
+        dense
     >
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
+      <v-btn text >
+        新建测试用例
+      </v-btn>
     </v-app-bar>
     <v-main>
-      <v-container class="d-flex grey lighten-5" fluid>
-    <v-row
-      no-gutters
-      style="flex-wrap: nowrap;"
-    >
-      <v-col
-        cols="2"
-        class="flex-grow-0 flex-shrink-0"
-      >
-        <v-card
-          class="pa-2"
-          outlined
-          tile
+      <div class="d-flex flex-column grey lighten-5" style="height: 100%">
+        <v-row
+            no-gutters
+            style="flex-wrap: nowrap; height: 40%; background: beige"
         >
-          I'm 2 column wide
-        </v-card>
-      </v-col>
-      <v-col
-        cols="1"
-        style="min-width: 100px; max-width: 100%;"
-        class="flex-grow-1 flex-shrink-0"
-      >
-        <v-card
-          class="pa-2"
-          outlined
-          tile
-        >
-          I'm 1 column wide and I grow to take all the space
-        </v-card>
-      </v-col>
-      <v-col
-        cols="5"
-        style="min-width: 100px;"
-        class="flex-grow-0 flex-shrink-1"
-      >
-        <v-card
-          class="pa-2"
-          outlined
-          tile
-        >
-          I'm 5 column wide and I shrink if there's not enough space
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+          <v-col
+              cols="5"
+              class="flex-grow-0 flex-shrink-0 yellow pa-2"
+          >
+          <v-item-group mandatory active-class="case-selected">
+            <v-item v-slot="{active,toggle}">
+              <v-card  @click="toggle">
+                <v-card-title>这是一个标题</v-card-title>
+                <v-card-subtitle>这是一个描述</v-card-subtitle>
+                <v-card-actions>
+                  <v-btn text>运行</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-item>
+            <v-item v-slot="{active,toggle}">
+              <v-card  @click="toggle">
+                <v-card-title>这是一个标题</v-card-title>
+                <v-card-subtitle>这是一个描述</v-card-subtitle>
+                <v-card-actions>
+                  <v-btn text>运行</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-item>
+            <v-item v-slot="{active,toggle}">
+              <v-card  @click="toggle">
+                <v-card-title>这是一个标题</v-card-title>
+                <v-card-subtitle>这是一个描述</v-card-subtitle>
+                <v-card-actions>
+                  <v-btn text>运行</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-item>
+          </v-item-group>
+
+          </v-col>
+          <v-col
+              class="flex-grow-1 flex-shrink-0"
+          >
+            I'm 4 column widexxxx
+          </v-col>
+        </v-row>
+        <v-row no-gutters>
+          <v-col style="background: cadetblue">
+            hello
+          </v-col>
+        </v-row>
+      </div>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Vue from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default Vue.extend({
   name: 'App',
 
   components: {
-    HelloWorld,
+    HelloWorld
   },
 
   data: () => ({
     //
     drawer: null
-  }),
-});
+  })
+})
 </script>
+<style scoped>
+.case-selected.v-card,.case-selected .v-card__subtitle{
+  background-color: cadetblue !important;
+  color: white !important;;
+}
+
+.case-selected .v-btn{
+  color: white !important;;
+
+}
+</style>
